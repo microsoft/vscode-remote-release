1d009e6d94baace39ed7e590cb62fc198e1b3d82
Martin Aeschlimann <martinae@microsoft.com>
2022-02-11 18:11:00 +0100

    update script

--- scripts/try-server.sh
+++ scripts/try-server.sh
@@ -6,40 +6,40 @@
 [ "$VSCODE_TRACE" = true ] && set -x
 
 if [ ! "$(command -v wget)" ]; then
-	echo "ERROR: Failed to download the VS Code server. 'wget' not installed." 1>&2
-	echo "Please install wget:" 1>&2
-	echo "Debian/Ubuntu: sudo apt-get install wget" 1>&2
-	exit 14
+    echo "ERROR: Failed to download the VS Code server. 'wget' not installed." 1>&2
+    echo "Please install wget:" 1>&2
+    echo "Debian/Ubuntu: sudo apt-get install wget" 1>&2
+    exit 14
 fi
 
 if [ -f /etc/alpine-release ]; then
-	case $(uname -m) in
-		x86_64)
-			INSTALL="server-linux-x64-alpine";;
-		aarch64)
-			INSTALL="server-alpine-arm64-web";;
-		*)
-			echo "Unknown platform $(uname -m). Falling back to x86_64" 1>&2
-			INSTALL="server-linux-x64-alpine";;
-	esac
-	if ! apk info | grep -qxe 'libstdc++'; then
-		echo "libstdc++ is required to run the VS Code Server:"  1>&2
-		echo "Please open an Alpine shell and run 'apk update && apk add libstdc++'" 1>&2
-		exit 12
-	fi
+    case $(uname -m) in
+        x86_64)
+            INSTALL="server-linux-x64-alpine";;
+        aarch64)
+            INSTALL="server-alpine-arm64-web";;
+        *)
+            echo "Unknown platform $(uname -m). Falling back to x86_64" 1>&2
+            INSTALL="server-linux-x64-alpine";;
+    esac
+    if ! apk info | grep -qxe 'libstdc++'; then
+        echo "libstdc++ is required to run the VS Code Server:"  1>&2
+        echo "Please open an Alpine shell and run 'apk update && apk add libstdc++'" 1>&2
+        exit 12
+    fi
 else
-	case $(uname -m) in
-		x86_64)
-			INSTALL="server-linux-x64-web";;
-		aarch64)
-			INSTALL="server-linux-arm64-web";;
-		*)
-			echo "Unknown platform $(uname -m). Falling back to x86_64" 1>&2
-			INSTALL="server-linux-x64-web";;
-	esac
+    case $(uname -m) in
+        x86_64)
+            INSTALL="server-linux-x64-web";;
+        aarch64)
+            INSTALL="server-linux-arm64-web";;
+        *)
+            echo "Unknown platform $(uname -m). Falling back to x86_64" 1>&2
+            INSTALL="server-linux-x64-web";;
+    esac
 fi
 
-BUILD_INFO=`curl -s -X GET https://update.code.visualstudio.com/api/update/$INSTALL/insider/latest`
+BUILD_INFO=`wget -qO- https://update.code.visualstudio.com/api/update/$INSTALL/insider/latest`
 
 DOWNLOAD_URL=$(echo $BUILD_INFO | sed -e 's/.*"url"\:\"\([^\"]\+\)\".*/\1/m')
 NAME=$(echo $BUILD_INFO | sed -e 's/.*"name"\:\"\([^\"]\+\)\".*/\1/m')
@@ -56,6 +56,8 @@ if [ ! -d "$SERVER_BUILD_DIR" ]; then
     mkdir -p "$SERVER_BUILDS_DIR"
   else
     # Remove the previous installation attempts
+    echo "Removing previous installations...";
+    rm -rf "$VSCODE_REMOTE_BIN"/????????????????????????????????????????
     rm -rf "$SERVER_BUILDS_DIR"/????????????????????????????????????????-??????????
     rm -rf "$SERVER_BUILDS_DIR"/????????????????????????????????????????-??????????.tar.gz
   fi
@@ -66,9 +68,9 @@ if [ ! -d "$SERVER_BUILD_DIR" ]; then
   SERVER_TAR_FILE="$SERVER_BUILD_DIR-$(date +%s).tar.gz"
   wget -O "$SERVER_TAR_FILE" "$DOWNLOAD_URL"
   if [ $? -eq 5 ]; then
-	echo "Please install missing certificates." 1>&2
-	echo "Debian/Ubuntu:  sudo apt-get install ca-certificates" 1>&2
-	exit 13
+    echo "Please install missing certificates." 1>&2
+    echo "Debian/Ubuntu:  sudo apt-get install ca-certificates" 1>&2
+    exit 13
   fi
 
   # Unpack the .tar.gz file to a temporary folder name
@@ -90,4 +92,4 @@ else
   CONNECTION_TOKEN=$(cat $CONNECTION_TOKEN_FILE)
 fi
 
-$SERVER_BUILD_DIR/bin/code-server-insiders --connection-token-file=$CONNECTION_TOKEN_FILE --port=7777 --host=localhost
+$SERVER_BUILD_DIR/bin/code-server-insiders --connection-token-file=$CONNECTION_TOKEN_FILE --port=7777 "$@"
