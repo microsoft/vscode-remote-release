ce1c295ee809e894773738c95ee6ee7c9f891bd8
Martin Aeschlimann <martinae@microsoft.com>
2022-03-11 15:27:42 +0100

    update windows scripts

--- scripts/start-server.ps1
+++ scripts/start-server.ps1
@@ -1,6 +1,9 @@
 #
 # Copyright (c) Microsoft Corporation. All rights reserved.
 #
+$OldProgressPreference = $ProgressPreference
+$ProgressPreference = 'SilentlyContinue' # turn off progress reporting as it slows down Invoke-WebRequest
+
 $Install = "server-win32-x64-web"
 
 $Url = "https://update.code.visualstudio.com/api/update/$Install/stable/latest"
@@ -15,36 +18,35 @@ $ServerBuildsDir="$ServerDataDir\bin-web"
 $ServerBuildDir="$ServerBuildsDir\$Commit"
 
 if (!(Test-Path -Path "$ServerBuildDir")){
-    if (!(Test-Path -Path "$ServerBuildsDir")){
-        [void](New-Item -Type Directory -Path "$ServerBuildsDir")
-    } else {
-        Write-Host "Removing previous installations..."
-        Remove-Item -Recurse -Force -Path "$ServerBuildsDir\????????????????????????????????????????"
-        Remove-Item -Recurse -Force -Path "$ServerBuildsDir\????????????????????????????????????????-??????????"
-        Remove-Item -Path "$ServerBuildsDir\????????????????????????????????????????-??????????.zip"
-    }
+	if (!(Test-Path -Path "$ServerBuildsDir")){
+		[void](New-Item -Type Directory -Path "$ServerBuildsDir")
+	} else {
+		Write-Host "Removing previous installations..."
+		Remove-Item -Recurse -Force -Path "$ServerBuildsDir\????????????????????????????????????????"
+		Remove-Item -Recurse -Force -Path "$ServerBuildsDir\????????????????????????????????????????-??????????"
+		Remove-Item -Path "$ServerBuildsDir\????????????????????????????????????????-??????????.zip"
+	}
 
-    $DownloadTimestamp = [int] (Get-Date -UFormat %s)
+	$DownloadTimestamp = [int] (Get-Date -UFormat %s)
 
-    # Download the build
-    Write-Host Downloading $Install $Name to $ServerBuildDir...
-    $ServerZipFile = "$ServerBuildDir-$DownloadTimestamp.zip"
-    $OldProgressPreference = $ProgressPreference
-    $ProgressPreference = 'SilentlyContinue' # turn off progress reporting as it slows down Invoke-WebRequest
-    Invoke-WebRequest -Uri $DownloadURL -OutFile $ServerZipFile
-    $ProgressPreference = $OldProgressPreference
+	# Download the build
+	Write-Host Downloading $Install $Name to $ServerBuildDir...
+	$ServerZipFile = "$ServerBuildDir-$DownloadTimestamp.zip"
+	Invoke-WebRequest -Uri $DownloadURL -OutFile $ServerZipFile
 
-    # Unpack the .zip file to a temporary folder name
-    Write-Host Extracting files...
+	# Unpack the .zip file to a temporary folder name
+	Write-Host Extracting files...
 
-    $ServerBuildTempDir = "$ServerBuildDir-$DownloadTimestamp"
-    Add-Type -Assembly "System.IO.Compression.Filesystem" # Faster than using Expand-Archive
-    [System.IO.Compression.ZipFile]::ExtractToDirectory("$ServerZipFile", "$ServerBuildTempDir")
+	$ServerBuildTempDir = "$ServerBuildDir-$DownloadTimestamp"
+	Add-Type -Assembly "System.IO.Compression.Filesystem" # Faster than using Expand-Archive
+	[System.IO.Compression.ZipFile]::ExtractToDirectory("$ServerZipFile", "$ServerBuildTempDir")
  
-    # Finalize the install
-    Move-Item -Path "$ServerBuildTempDir\vscode-$Install" -Destination "$ServerBuildDir"
-    Remove-Item -Path "$ServerBuildTempDir"
-    Remove-Item -Path "$ServerZipFile"
+	# Finalize the install
+	Move-Item -Path "$ServerBuildTempDir\vscode-$Install" -Destination "$ServerBuildDir"
+	Remove-Item -Path "$ServerBuildTempDir"
+	Remove-Item -Path "$ServerZipFile"
 }
+
+$ProgressPreference = $OldProgressPreference
 
 Invoke-Expression -Command "$ServerBuildDir\bin\code-server.cmd $args" 
